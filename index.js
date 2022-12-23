a47ec6a405eb4dd178d2b8cdf79921c71678f65a
unknown <muduli230@gmail.com>
2022-12-23 12:57:11 +0530

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-hello Geolocation("hello google")
+// console.log("hello world");
+// //alert("get up");
+// console.warn("this is a warning");
+// console.error("This is an error");
+/*hello world
+this is a best eworld*/
+//data type :-
+
+//num data type
+var number1 = 20;
+var number2 = 56;
+console.log(number1 + number2);
+
+//str Data type
+var str1 = "this is a str note"
+var str2 = "this is a str best str note"
+console.log("str1, str2")
+//objects data type
+var marks = {
+    rabi: 20,
+    hari:23,
+    kiran:25,
+}
+console.log(marks);
+//Booleans
+var a = true;
+var b = false;
+console.log(a,b);
+
+//undefined
+var und = undefined;
+console.log(und);
+
+//null
+var a = null;
+console.log(a);
+
+//Arrays
+var arr = [1,2,3,4,5];
+console.log(arr)
+
+/*
+  There are two type of data types in javascript
+  1. Primitive data types: undefine, rull, number, string, boolean, symbol
+  2. reference data type: Arrays and object.
+*/
